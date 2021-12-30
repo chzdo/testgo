aa7baeb3a201f70b1dcd04732c2e9cebf11b6f1e
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2021-12-30 11:30:18 +0100

    added mongo

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -10,10 +18,25 @@ func main() {
 
 	router := http.NewServeMux()
 
+	mg, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongo://localhost:27017/test"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
+		cx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+
+		defer cancel()
+
+		list, err := mg.ListDatabaseNames(cx, bson.M{}, options.ListDatabases().SetNameOnly(true))
 		rw.WriteHeader(200)
-		rw.Write([]byte(`{status:200}`))
+
+		fmt.Println(err)
+
+		json.NewEncoder(rw).Encode(list)
+
 	})
 
 	http.ListenAndServe(":"+port, router)
